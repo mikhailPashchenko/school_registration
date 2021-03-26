3ffa59f7933cfcaed8978ac48861581cb8867ace
mikhailPashchenko <m.pashchenko.92@gmail.com>
2021-03-27 02:04:46 +0300

    first success version

--- registration.py
+++ registration.py
@@ -11,8 +11,8 @@ url = "https://firstclass.uslugi.mosreg.ru/summerform"
 girl = 'Жен'
 boy = 'Муж'
 
-rus = 'Свидетельство о рождении'
-alien = 'Свидетельство о рождении иностранного образца'
+rus = ' Свидетельство о рождении '
+alien = ' Свидетельство о рождении иностранного образца '
 
 child_surname = "Тестовый"
 child_name = "Ученик"
@@ -21,16 +21,20 @@ child_gender = girl
 birth_date = "01.02.2015"
 birth_place = "Россия, г. Челябинск"
 
-doc_type = alien
+doc_type = rus
 doc_serial =  "II-АБ"
 doc_number = "123456"
 doc_date = "01.02.2015"
 doc_depart_name = "Каким-то ЗАГСом"
 
-region_name = "Ленинский Район"
-city_name = "Жуковский Город"
-street_name = "Безымянный Проезд"
-house_number = "1a"
+region_name = ""
+city_name = " Жуковский Город "
+street_name = " Безымянный Проезд "
+house_number = " 1а Дом "
+flat_number = "10"
+
+municipal_name = ' Городской округ Одинцовский '
+school_name = ' МБОУ Одинцовская лингвистическая гимназия '
 
 chrome = webdriver.Chrome()
 chrome.get(url)
@@ -39,7 +43,6 @@ chrome.get(url)
 def keep_agree():
     check = chrome.find_element(By.TAG_NAME, "app-ui-checkbox")
     check.location_once_scrolled_into_view
-    #check.execute_script('arguments[0].scrollIntoView(true);', check)
     check.click()
     # go to child info page
     chrome.find_element(By.ID, "next-button").click()
@@ -49,11 +52,9 @@ def gender(g):
     if g in value.text:
         return
     elif g == boy:
-        #chrome.find_element(By.XPATH, "//mat-select[@required_id = 'id_childGender']").click()
         value.click()
         chrome.find_element(By.ID, "mat-option-1").click()
     elif g == girl:
-        #chrome.find_element(By.XPATH, "//mat-select[@required_id = 'id_childGender']").click()
         value.click()
         chrome.find_element(By.ID, "mat-option-2").click()
 
@@ -113,29 +114,63 @@ def birth():
 
     upload_birth()    
 
-def select_list(field_id, search_id, search):
+def select_list(by, selector, search):
     if search == "":
         return
-    field = chrome.find_element_by_id(field_id)
+
+    field = chrome.find_element(by, selector)
     field.location_once_scrolled_into_view
     field.click()
-    search_str = chrome.find_element_by_id(search_id)
-    search_str.send_keys(search)
-    wait = WebDriverWait(chrome, 3)
-    element = wait.until(EC.element_to_be_clickable((By.ID,search_id)))
-    element.send_keys(Keys.ENTER)
+    chrome.find_element(By.XPATH, "//span[text() = '%s']" % search).click()
 
 def region():
-    select_list("id_district", "mat-input-7", region_name)
+    select_list(By.ID, "id_district", region_name)
 
 def city():
-    select_list("id_city", "mat-input-9", city_name)
+    select_list(By.ID, "id_city", city_name)
 
 def street():
-    select_list("id_street", "mat-input-13", street_name)
+    select_list(By.ID, "id_street", street_name)
 
 def house():
-    select_list("id_house", "mat-input-17", house_number)
+    select_list(By.ID, "id_house", house_number)
+
+def flat():
+    inp = chrome.find_element(By.ID, "id_regFlat")
+    inp.send_keys(flat_number)
+
+def municipal():
+    select_list(By.XPATH, "//div[@data-name = 'munObr']", municipal_name)
+    #field = chrome.find_element(By.XPATH, "//div[@data-name = 'munObr']")
+    #field.location_once_scrolled_into_view
+    #field.click()
+    #chrome.find_element(By.XPATH, "//span[text() = '%s']" % municipal_name).click()
+
+def school():
+    field = chrome.find_element(By.XPATH, "//div[@data-name = 'selectedSchools']")
+    field.location_once_scrolled_into_view
+    field.click()
+    chrome.find_element(By.XPATH, "//span[text() = '%s']" % school_name).click()
+
+def year():
+    field = chrome.find_element(By.ID, 'id_schoolYear_fake')
+    if field.is_enabled():
+        field.send_keys(Keys.CONTROL + "a")
+        field.send_keys(Keys.DELETE)
+        field.send_keys("2021/2022")
+
+def school_data():
+    municipal()
+    school()
+    year()
+
+def submit():
+    but = chrome.find_element(By.ID, "next-button")
+    if but.is_enabled():
+        #but.click()
+        print("    Success!     ")
+    else:
+        pdb.set_trace()
 
 keep_agree()
 personal_info()
@@ -144,5 +179,8 @@ region()
 city()
 street()
 house()
+flat()
+school_data()
+submit()
 pdb.set_trace()
 chrome.close()
